8ffb7ddf622e11784d9d669979404374192ceb44
JUCEAS <juceas19@gmail.com>
2026-09-20 20:35:16 -0600

    Update cache name to apicampo-cache-v5

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Objetivo: que la app abra y funcione aunque no haya internet,
    incluso si el teléfono estuvo apagado o sin señal desde la instalación. */
 
-var CACHE_NAME = "apicampo-cache-v4";
+var CACHE_NAME = "apicampo-cache-v5";
 
 var APP_SHELL = [
   "./manifest.json",
