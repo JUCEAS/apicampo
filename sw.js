e4cabccebb4f1d9998dcaad1722125e7f5dfab80
JUCEAS <juceas19@gmail.com>
2026-09-20 22:47:09 -0600

    Bump CACHE_NAME to apicampo-cache-v10
    Updated cache version to v10 for service worker.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Objetivo: que la app abra y funcione aunque no haya internet,
    incluso si el teléfono estuvo apagado o sin señal desde la instalación. */
 
-var CACHE_NAME = "apicampo-cache-v9";
+var CACHE_NAME = "apicampo-cache-v10";
 
 var APP_SHELL = [
   "./manifest.json",
