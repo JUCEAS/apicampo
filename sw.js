36fede6ca565cc460eab3d5350f369b641b9c0ba
JUCEAS <juceas19@gmail.com>
2026-09-23 19:10:12 -0600

    Update CACHE_NAME to version 13

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Objetivo: que la app abra y funcione aunque no haya internet,
    incluso si el teléfono estuvo apagado o sin señal desde la instalación. */
 
-var CACHE_NAME = "apicampo-cache-v12";
+var CACHE_NAME = "apicampo-cache-v13";
 
 var APP_SHELL = [
   "./manifest.json",
